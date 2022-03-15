4e4207ba28e258f3e3f3d62fa2db635ce771e7ad
0980491 <86085545+0980491@users.noreply.github.com>
2022-03-14 21:58:24 -0500

    Update index.php

--- union temporal isekai/index.php
+++ union temporal isekai/index.php
@@ -15,7 +15,7 @@
       </div>
     </div>
     <div class="carousel-item">
-      <img src="https://cdn.discordapp.com/attachments/715927167531483146/953120184393162862/68501052-pixels-de-8-bits-arriC3A8re-plan-illustration-vectorielle.png" class="d-block w-100" alt="...">
+      <img src="https://cdn.discordapp.com/attachments/715927167531483146/953121786864758815/fondo-caos-puntos-pixeles-colores_1017-30635.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Kamazon Elite</h5>
         <a class="btn btn-light" href="#" role="button">Link</a>
